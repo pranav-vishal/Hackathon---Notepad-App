a9ccf6a26862091cde90d2d7c1a544f6ed2dcc66
Abu-Talha-24 <itsabutalha@gmail.com>
2022-09-26 17:16:56 +0530

    Basic Functionalities added

--- notepad.js
+++ notepad.js
@@ -1,6 +1,18 @@
-let text = getElementbyId("")
+const boldBtn = document.querySelector("#bold-btn")
+const underlineBtn = document.querySelector("#underline-btn")
+const copyBtn = document.querySelector("#copy-btn")
 
 
-let selectedText = window.getSelection().toString();
+boldBtn.addEventListener('click', () => {
+  document.execCommand("bold");
+})
 
+underlineBtn.addEventListener('click', () => {
+  document.execCommand("underline");
+})
 
+copyBtn.addEventListener('click', () => {
+  let text = document.querySelector('.content').innerText;
+  console.log(text);
+navigator.clipboard.writeText(text)
+})
